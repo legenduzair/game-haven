1bdadb5686589437ac3ff6bd337b46ce4d881ae8
Uzair Oomer <uzair.oomer@jdplc.com>
2024-01-01 19:14:19 +0000

    Remove user models interface

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,21 +1,12 @@
-import { Document, Schema, models, model } from "mongoose";
-
-export interface IUser extends Document {
-    clerkId: string;
-    email: string;
-    username: string;
-    firstName: string;
-    lastName: string;
-    photo: string;
-}
+import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkId: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    photo: { type: String, required: true },
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: {type: String, required: true },
+  photo: { type: String, required: true },
 })
 
 const User = models.User || model('User', UserSchema);
