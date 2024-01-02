f79731661f1dd41fb97d4d6e071daa51232d8def
Uzair Oomer <uzair.oomer@jdplc.com>
2024-01-02 16:55:08 +0000

    add event form using shadcn form library and amend validation for input fields

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,13 +1,59 @@
 "use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator"
+import { eventDefaultValues } from "@/constants"
+
+
+
 type EventFormProps = {
     userId: string,
     type: "Create" | "Update"
 }
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+  
+  const initialValues = eventDefaultValues;
+
+  // Define your form.
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  })
+ 
+  // Define a submit handler.
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    console.log(values)
+  }
+
   return (
-    <div>EventForm {type}</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
