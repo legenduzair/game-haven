ebea03306ba8fe3e44d22492dee676a0e13126c3
Uzair Oomer <uzair.oomer@jdplc.com>
2024-01-08 17:10:00 +0000

    Add dates form field by using reactdatepicker and add price field

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -16,6 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox"
 
 import { eventFormSchema } from "@/lib/validator";
 import { eventDefaultValues } from "@/constants";
@@ -24,6 +25,9 @@ import Dropdown from "./Dropdown";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
 import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Check } from "lucide-react";
 
 type EventFormProps = {
   userId: string;
@@ -127,17 +131,148 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             render={({ field }) => (
               <FormItem className="w-full">
                 <FormControl>
-                  <div className="flex flex-center h-[54px] w-full overflow-hidden
-                  rounded-full bg-gray-50 px-4 py-2">
+                  <div
+                    className="flex flex-center h-[54px] w-full overflow-hidden
+                  rounded-full bg-gray-50 px-4 py-2"
+                  >
                     <Image
                       src="/assets/icons/location-grey.svg"
                       alt="calendar"
                       width={24}
-                      height={24} />
+                      height={24}
+                    />
                     <Input
                       placeholder="Event Location or Online"
                       {...field}
                       className="input-field"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="startDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div
+                    className="flex flex-center h-[54px] w-full overflow-hidden
+                  rounded-full bg-gray-50 px-4 py-2"
+                  >
+                    <Image
+                      src="/assets/icons/calendar.svg"
+                      alt="calendar"
+                      width={24}
+                      height={24}
+                      className="filter-grey"
+                    />
+                    <p className="ml-3 whitespace-nowrap text-gray-600">
+                      Start Date:
+                    </p>
+                    <DatePicker
+                      selected={field.value}
+                      onChange={(date: Date) => field.onChange(date)}
+                      showTimeSelect
+                      timeInputLabel="Time:"
+                      dateFormat="dd/MM/yyyy HH:mm aa"
+                      wrapperClassName="datePicker"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="endDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div
+                    className="flex flex-center h-[54px] w-full overflow-hidden
+                  rounded-full bg-gray-50 px-4 py-2"
+                  >
+                    <Image
+                      src="/assets/icons/calendar.svg"
+                      alt="calendar"
+                      width={24}
+                      height={24}
+                      className="filter-grey"
+                    />
+                    <p className="ml-3 whitespace-nowrap text-gray-600">
+                      End Date:
+                    </p>
+                    <DatePicker
+                      selected={field.value}
+                      onChange={(date: Date) => field.onChange(date)}
+                      showTimeSelect
+                      timeInputLabel="Time:"
+                      dateFormat="dd/MM/yyyy HH:mm aa"
+                      wrapperClassName="datePicker"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div
+                    className="flex flex-center h-[54px] w-full overflow-hidden
+                  rounded-full bg-gray-50 px-4 py-2"
+                  >
+                    <Image
+                      src="/assets/icons/dollar.svg"
+                      alt="pounds"
+                      width={24}
+                      height={24}
+                      className="filter-grey"
+                    />
+                    <p className="ml-3 whitespace-nowrap text-gray-600">
+                      End Date:
+                    </p>
+                    <Input
+                      type="number"
+                      placeholder="Price"
+                      {...field}
+                      className="p-regular-16 border-0 bg-gray-50 outline-offset-0
+                    focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                    <FormField
+                      control={form.control}
+                      name="isFree"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div
+                              className="flex items-center"
+                            >
+                              <label htmlFor="isFree"
+                              className="whitespace-nowrap pr-3 leading-none
+                              peer-disabled:cursor-not-allowed
+                              peer-disabled:opacity-70">Free Ticket</label>
+                              <Checkbox id="isFree"
+                              className="mr-2 h-5 w-5 border-2 border-primary-500" />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
                   </div>
                 </FormControl>
