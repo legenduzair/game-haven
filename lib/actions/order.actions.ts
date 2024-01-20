35f3758e9ae851bafe8193d7dab1413e114b99f7
Uzair Oomer <uzair.oomer@jdplc.com>
2024-01-20 15:39:45 +0000

    implement stripe webhook and add code to add order to db

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
 import Stripe from 'stripe';
-import { CheckoutOrderParams } from "@/types";
+import { CheckoutOrderParams, CreateOrderParams } from "@/types";
 import { redirect } from 'next/navigation';
+import { connectToDatabase } from '../database';
+import { handleError } from '../utils';
+import Order from '../database/models/order.model';
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
 
@@ -37,3 +40,19 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
     throw error;
   }
 };
+
+export const createOrder = async (order: CreateOrderParams) => {
+  try {
+    await connectToDatabase();
+    
+    const newOrder = await Order.create({
+      ...order,
+      event: order.eventId,
+      buyer: order.buyerId,
+    });
+
+    return JSON.parse(JSON.stringify(newOrder));
+  } catch (error) {
+    handleError(error);
+  }
+}
